c5ee5d1fd4edd853fa08f126186cbe20374e916c
DarioDC95 <dario_95@live.com>
2022-11-25 18:47:08 +0100

    Created bonus-Folder

---
+++ bonus/js/script.js
@@ -0,0 +1,29 @@
+// Chiedere i kilometri da percorrere
+let kilometers = parseInt(prompt("Quanti chilometri desideri percorrere?"));
+
+// Chiedere l'età del passeggero
+let age = parseInt(prompt("Qual'è la sua età?"));
+
+// Definisco la costante del costo a kilometro
+const unit_prize = 0.21;
+
+// Definisco il prezzo normale
+let prize_ticket = kilometers * unit_prize;
+
+// Definisco gli sconti
+if (age < 18) {
+    prize_ticket = prize_ticket * 0.8;
+}
+
+else if (age > 65) {
+    prize_ticket = prize_ticket * 0.6;
+}
+
+else {
+    prize_ticket = prize_ticket;
+}
+
+// setto il massimo di due decimali
+let adjusted_prize_ticket = prize_ticket.toFixed(2);
+
+console.log(adjusted_prize_ticket);
