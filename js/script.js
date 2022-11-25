81b2166ab48d7f8f7e5af0fd4649eabdd22c151e
DarioDC95 <dario_95@live.com>
2022-11-25 17:34:04 +0100

    Inizio javascript

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// Chiedere i kilometri da percorrere
+let kilometers = parseInt((prompt("Quanti chilometri desideri percorrere?")), 10);
+console.log(kilometers)
+
+// Chiedere l'età del passeggero
+let age = parseInt(prompt("Qual'è la sua età?"));
+console.log(age)
+
+// Definisco la costante del costo a kilometro
+const unit_prize = 0.21;
+
+// Definisco il prezzo normale
+let prize_ticket = kilometers;
+
+// Definisco gli sconti
+if (age < 18) {
+    
+}
+
